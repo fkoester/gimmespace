396f45ff006a2b8eae18f97441ccb28f8b79ebc7
Fabian Köster <mail@fabian-koester.com>
2019-01-25 19:29:36 +0100

    Create initial schema version

---
+++ alembic/versions/f4307b9a50e7_initial_schema.py
@@ -0,0 +1,23 @@
+"""Initial Schema
+
+Revision ID: f4307b9a50e7
+Revises: 
+Create Date: 2019-01-25 19:29:30.671566
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = 'f4307b9a50e7'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass

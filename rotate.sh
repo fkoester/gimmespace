b5febc72c23fd39817a57b42f7c4d1fa0f7e3644
Fabian Köster <mail@fabian-koester.com>
2021-02-21 17:38:41 +0100

    Change snapshots dir back

--- rotate.sh
+++ rotate.sh
@@ -2,7 +2,7 @@
 
 NUM=${1}
 
-#DIR="/run/media/fabian/gopro/Gopro/snapshots/"
-DIR="/mnt/gopro-hdd/Gopro/snapshots/"
+DIR="/run/media/fabian/gopro/Gopro/snapshots/"
+#DIR="/mnt/gopro-hdd/Gopro/snapshots/"
 
 mogrify -rotate 180 ${DIR}/vlcsnap-GX0*1${NUM}.MP4*
